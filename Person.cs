ee0d25d733d45256703ecd570eba242af84b6218
CarlosAlas <48497546+CarlosAlas-CLA@users.noreply.github.com>
2019-04-18 19:07:07 -0400

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleAppClass
+{
+    public class Person
+    {//Constructor
+        public string name;
+
+        public Person(string n)
+        {
+            name = n;
+        }
+        //method
+        public void setNa(string newN)
+        {
+            name = newN;
+        }
+    }
+}
