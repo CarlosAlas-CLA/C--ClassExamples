7123364855123176ad0120f68df55ec13cce8ddd
CarlosAlas <48497546+CarlosAlas-CLA@users.noreply.github.com>
2019-09-26 17:53:48 -0400

    Update SendEmails.cs

--- SendEmails.cs
+++ SendEmails.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace TestCodeLibrary
     {
-    class SendEmails
+      class SendEmails
         {
-        }
-    }
+        public static void SendReports(string vProgramName, string vProgramDir, string vLogPath, string vFromEmail, string  [ ] vToList , string[]  vCCList ,string []vBCCList,  string vSubject, string vMailBody  , string[] vAttachment) { }
+        
+
+       
+    
+public static void SendExceptionOccur(string vProgramName,string  vProgramDir,string vLogPath,string exception0, string exception1, string exception2, string exception3) { }
+}
+
+}
