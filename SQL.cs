b30f493b9fbbc1cbbacffc3f15b4a3aef856175b
CarlosAlas <48497546+CarlosAlas-CLA@users.noreply.github.com>
2019-09-26 17:50:03 -0400

    Add files via upload

---
+++ SQL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCodeLibrary
+    {
+    class SQL
+        {
+        public static void SqlToExcel() {
+            }
+    
+        }
+    }
